12f190f3026215eb5008c374f179771f95cef032
Albert <>
2019-03-31 16:04:49 -0400

    cli_1

--- CLI.py
+++ CLI.py
@@ -1,17 +1,21 @@
 import Manager
 commands = {0:"help",
             1:"create_service_from_yml"}
-print("All Commands:")
-for c in commands:
-    print(str(c)+": "+commands.get(c))
+
+def print_commands():
+    print("All Commands:")
+    for c in commands:
+        print(str(c) + ": " + commands.get(c))
 
 manager = Manager.Manager("http://127.0.0.1:2379","docker-compose.yml")
 
+
+print_commands()
 while True:
     user_input = input("Enter: ")
 
     if(user_input=="help"):
-        print("here")
+        print_commands()
     elif(user_input =="exit"):
         break
     elif(user_input == "create_service_from_yml"):

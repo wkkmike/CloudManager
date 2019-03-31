2c38cd0968e248bc3d79b762387f7b65f2383f8c
wkkmike <michaelwang404@hotmail.com>
2019-03-31 16:41:55 -0400

    First part of manager

--- Manager.py
+++ Manager.py
@@ -15,11 +15,17 @@ class Manager:
     def __etcd_get_prefix(self, prefix):
         return subprocess.run(args=["etcdctl", "--endpoints=" + self.etcd, "get",
                                     "--prefix", prefix],capture_output=True).\
-            stdout.decode("utf-8").strip().split()
+            stdout.decode("utf-8").strip().split("\n")
 
     def __etcd_get(self, key):
         return subprocess.run(args=["etcdctl", "--endpoints=" + self.etcd, "get", key],
                         capture_output=True).stdout.decode("utf-8").strip()
+
+    def __etcd_del(self, key):
+        subprocess.run(args=["etcdctl", "del", key])
+
+    def __etcd_del_prefix(self, prefix):
+        subprocess.run(args=["etcdctl", "del", "--prefix", prefix])
 
     def __get_container_list_from_etcd(self):
         return_value = subprocess.run(args=["etcdctl", "--endpoints=" + self.etcd, "get", "--prefix", "''"],
@@ -63,9 +69,32 @@ class Manager:
                 command += "  " + line
                 line = file.readline()
             command += "    labels:\"- traefik.frontend.rule=Host:whoami.docker.localhost\"\n"
-        if not self.__etcd_put("image_" + name, 0):
+        if not self.__etcd_put("image_" + name, "0"):
             return False
 
         with open(self.config, "a") as file:
             file.write(command)
         return True
+
+    def list_all_service(self):
+        list = self.__etcd_get_prefix("image_")
+        answer = "{:30s}{:30s}".format("Service Name", "Number of instances") + "\n"
+        i=0
+        for item in list:
+            if i%2 == 0:
+                answer += "{:30s}".format(item[6:])
+            else:
+               answer += "{:30s}".format(item) + "\n"
+        return answer
+
+    def run_service(self, name, amount):
+        output = subprocess.run(["docker-compose","up", "--scale", name+"="+amount, "-d"], capture_output=True)
+        self.__etcd_put("image_" + name, amount)
+        id_list = subprocess.run(["docker", "ps", "-qf", "\"name="+name+"\""], capture_output=True)\
+            .stdout.strip().decode("utf-8").split("\n")
+        self.__etcd_del_prefix("container_" + name)
+        i=1
+        for id in id_list:
+            self.__etcd_put("container_" + name + "_" + str(i), id)
+            i += 1
+        return output.stdout.decode("utf-8")
